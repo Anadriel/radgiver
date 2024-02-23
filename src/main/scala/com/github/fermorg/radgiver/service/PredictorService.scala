fff3eb174f3d612748c839867fce24bb8f732b40
Anna Iakovleva <massadant@yandex.ru>
2024-02-23 01:50:54 +0100

    feat: construct a batch after prediction

--- src/main/scala/com/github/fermorg/radgiver/service/PredictorService.scala
+++ src/main/scala/com/github/fermorg/radgiver/service/PredictorService.scala
@@ -3,7 +3,6 @@ package com.github.fermorg.radgiver.service
 import com.github.fermorg.radgiver.config.PredictorConfig
 import com.github.fermorg.radgiver.model.http.PromptedPrediction
 import com.github.fermorg.radgiver.service.DeichmanApiService.TimeWindow
-
 import zio.{Chunk, RLayer, Scope, ZIO, ZLayer}
 import zio.json.*
 
@@ -45,43 +44,57 @@ object PredictorService {
         eventsForProcessing = fetchedEvents
           .filterNot(event => processedEventsIds.contains(event.id))
           .sortBy(_.startTime)
-          .take(realBatchSize)
           .map(_.id)
         _ <- ZIO.logInfo(
-          s"Processing ${eventsForProcessing.size} events: ${eventsForProcessing.mkString("[", ", ", "]")}"
+          s"Available for processing ${eventsForProcessing.size} events: ${eventsForProcessing
+              .mkString("[", ", ", "]")}"
         )
-        nextState = processedEventsIds.intersect(fetchedEventIds).union(eventsForProcessing.toSet)
-        result <- predictionsAboveThreshold(eventsForProcessing)
-        _ <- ZIO.logInfo(s"Got ${result.size} predictions above threshold")
+        result <- predictionsAndNewState(eventsForProcessing, realBatchSize)
+        nextState = processedEventsIds.intersect(fetchedEventIds).union(result._2.toSet)
+        _ <- ZIO.logInfo(s"Got ${result._1.size} predictions above threshold")
         _ <- ZIO.logInfo(
           s"Next state (${nextState.size} events): ${nextState.mkString("[", ", ", "]")}"
         )
         _ <- state.setState(nextState)
-      } yield result
+      } yield result._1
     }
 
-    private def predictionsAboveThreshold(
-      eventIds: Chunk[String]
-    ): ZIO[Scope, Throwable, List[PromptedPrediction]] = eventIds
-      .mapZIOPar { eventId =>
-        deichman
-          .getEvent(eventId)
-          .flatMap(event => vertexAI.predictChatPrompt(event.description))
-          .tap(prediction =>
-            ZIO.logInfo(
-              s"Event $eventId processed with the result: ${prediction.fold("No prediction")(_.toJson)}"
+    private def predictionsAndNewState(
+      newEventIds: Chunk[String],
+      realBatchSize: Int,
+      resultedPredictions: List[PromptedPrediction] = List.empty,
+      processedEvents: List[String] = List.empty,
+    ): ZIO[Any, Throwable, (List[PromptedPrediction], List[String])] =
+      newEventIds.headOption match {
+        case Some(eventId) if resultedPredictions.size < realBatchSize =>
+          eventProcessing(eventId).flatMap { op =>
+            predictionsAndNewState(
+              newEventIds.tail,
+              realBatchSize,
+              op.fold(resultedPredictions)(p => resultedPredictions :+ p),
+              processedEvents :+ eventId,
             )
-          )
+          }
+        case _ =>
+          ZIO.succeed((resultedPredictions, processedEvents))
       }
-      .map {
-        _.flatMap {
+
+    private def eventProcessing(eventId: String): ZIO[Any, Throwable, Option[PromptedPrediction]] =
+      deichman
+        .getEvent(eventId)
+        .flatMap(event => vertexAI.predictChatPrompt(event.description))
+        .tap { prediction =>
+          ZIO.logInfo(
+            s"Event $eventId processed with the result: ${prediction.fold("No prediction")(_.toJson)}"
+          )
+        }
+        .map {
           case Some(promptedPrediction)
               if promptedPrediction.percentage >= config.minimalEventMatchProbability =>
             Some(promptedPrediction)
           case _ =>
             None
-        }.toList
-      }
+        }
 
   }
 
